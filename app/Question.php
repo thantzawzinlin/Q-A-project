e840594c1e01e402462fb6673ba92a86e519b52a
Thant Zaw <thantzawzinlin@gmail.com>
2020-02-07 15:09:24 +0630

    Faker factory and seed

--- app/Question.php
+++ app/Question.php
@@ -1,20 +1,20 @@
 <?php
 
 namespace App;
-
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    protected $fillable=['title',body];
+    protected $fillable=['title','body'];
     public function user()
     {
         return $this->belongsTo(User::class);
     }
     public function setTitleAttribute($value)
     {
-        $this->Attributes['title']=$value;
-        $this->Attributes['slug']=Str::slug($value);
+        $this->attributes['title']=$value;
+        $this->attributes['slug']=Str::slug($value);
     }
     
 }
